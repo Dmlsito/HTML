35df27da6e3e04fbc0e268c6c4d0d0b675499735
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2023-02-16 19:45:59 +0100

    Pesca

---
+++ pesca/lib/secondScreem.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class secondScreem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(child: Container(child: Text("Hola que tal"))));
+  }
+}
